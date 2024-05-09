e5209f5f1f8331c64e91a47e8a9ceca9427acdac
moreira@zaia.com.br <moreira@zaia.com.br>
2024-05-09 16:05:29 -0300

    test

--- homeManagerModules/features/git.nix
+++ homeManagerModules/features/git.nix
@@ -1,22 +1,52 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... 
 
-{
-  home.packages = [ pkgs.git ];
+}: let
+  cfg = config.myNixOS;
+in {
 
-  programs.git = {
-    enable = true;
-    userName = config.userSettings.name;
-    userEmail = config.userSettings.email;
-    extraConfig = {
-      init.defaultBranch = "main";
-      safe.directory = "/home/" + config.userSettings.username + "/.dotfiles";
-    };
+  options = {
+    myNixOS = {
+      enable = mkEnableOption "My NixOS";
+      userSettings = {
+        enable = mkEnableOption "User settings";
+        username = mkOption {
+          type = types.str;
+          default = "";
+          description = "The username of the user";
+        };
+        name = mkOption {
+          type = types.str;
+          default = "";
+          description = "The name of the user";
+        };
+        email = mkOption {
+          type = types.str;
+          default = "";
+        };
+      };
+    }; 
   };
 
-  home.file."GIT/.keep".text = ''
-    Welcome to the Insane world ! 
-  
-  '';
+  config = {
+
+    home.packages = [ pkgs.git ];
+
+    programs.git = {
+      enable = true;
+      userName = config.userSettings.name;
+      userEmail = config.userSettings.email;
+      extraConfig = {
+        init.defaultBranch = "main";
+        safe.directory = "/home/" + config.userSettings.username + "/.dotfiles";
+      };
+    };
+
+    home.file."GIT/.keep".text = ''
+      Welcome to the Insane world ! 
+    
+    '';
+
+  };
 
 
 }
