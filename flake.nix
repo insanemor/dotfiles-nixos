75da47afd2c068da6e0c7a4119f699e9b4ec0964
moreira@zaia.com.br <moreira@zaia.com.br>
2024-05-09 20:10:47 -0300

    add comment

--- flake.nix
+++ flake.nix
@@ -29,11 +29,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    ##### Usado no repo privado
+    # sops-nix = {
+    #   url = "github:Mic92/sops-nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     
   };
 
